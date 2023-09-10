f0d8c8cc6452886527e5cea624fd7f078395cf4c
Saurav Negi <112284209+sane5805@users.noreply.github.com>
2023-09-10 17:04:52 -0600

    Update writer.c : test commit

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -6,15 +6,15 @@
 
 int main(int argc, char *argv[]) {
 
-    //openlog("log", LOG_PID, LOG_USER);
+    openlog("log", LOG_PID, LOG_USER);
     
     // Check if both arguments are provided
     if (argc != 3) {
-        fprintf(stderr, "Usage: %s <writefile> <writestr>\n", argv[0]);
-        //syslog(LOG_ERR, "Usage: ./writer <writefile> <writestr>\n");
+        //fprintf(stderr, "Usage: %s <writefile> <writestr>\n", argv[0]);
+        syslog(LOG_ERR, "Usage: ./writer <writefile> <writestr>\n");
         
-        exit(EXIT_FAILURE);
-        //return 1;
+        //exit(EXIT_FAILURE);
+        return 1;
     }
 
     // Assign arguments to variables
@@ -23,45 +23,48 @@ int main(int argc, char *argv[]) {
 
     // Check if writefile is not empty
     if (writefile == NULL || strlen(writefile) == 0) {
-        fprintf(stderr, "Error: 'writefile' argument is empty.\n");
-        //syslog(LOG_ERR, "Error: 'writefile' argument is empty.\n");
+        //fprintf(stderr, "Error: 'writefile' argument is empty.\n");
+        syslog(LOG_ERR, "Error: 'writefile' argument is empty.\n");
         
-        exit(EXIT_FAILURE);
-        //return 1;
+        //exit(EXIT_FAILURE);
+        return 1;
     }
 
     // Check if writestr is not empty
     if (writestr == NULL || strlen(writestr) == 0) {
-        fprintf(stderr, "Error: 'writestr' argument is empty.\n");
-        //syslog(LOG_ERR, "Error: 'writestr' argument is empty.\n");
+        //fprintf(stderr, "Error: 'writestr' argument is empty.\n");
+        syslog(LOG_ERR, "Error: 'writestr' argument is empty.\n");
         
-        exit(EXIT_FAILURE);
-        //return 1;
+        //exit(EXIT_FAILURE);
+        return 1;
     }
 
     // Open the file for writing
     FILE *file = fopen(writefile, "w");
     if (file == NULL) {
-        fprintf(stderr, "Error: Failed to open file '%s' for writing.\n", writefile);
-        
-        exit(EXIT_FAILURE);
-        //return 1;
+        //fprintf(stderr, "Error: Failed to open file '%s' for writing.\n", writefile);
+        syslog(LOG_ERR, "Error: Failed to open file.");
+            
+        //exit(EXIT_FAILURE);
+        return 1;
     }
 
     // Write the content to the file
     if (fprintf(file, "%s", writestr) < 0) {
-        fprintf(stderr, "Error: Failed to write to file '%s'.\n", writefile);
+        //fprintf(stderr, "Error: Failed to write to file '%s'.\n", writefile);
+        syslog(LOG_ERR, "Error: Failed to write to file.");
+        
         fclose(file);
         
-        exit(EXIT_FAILURE);
-        //return 1;
+        //exit(EXIT_FAILURE);
+        return 1;
     }
 
     // Close the file
     fclose(file);
 
     // Log the message with syslog
-    openlog("writer", LOG_PID, LOG_USER);
+    //openlog("writer", LOG_PID, LOG_USER);
     syslog(LOG_DEBUG, "Writing %s to %s", writestr, writefile);
     closelog();
 
