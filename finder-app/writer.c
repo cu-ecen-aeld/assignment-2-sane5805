ac8802a596c78a2bb294aba1ab086e00217c175f
Saurav Negi <112284209+sane5805@users.noreply.github.com>
2023-09-10 21:16:19 -0600

    Update writer.c : commenting better

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -1,73 +1,108 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <syslog.h>
+/*==========================================================================
+  File Name: writer.c
+  This file contains the writer code for writing a string to a file.
 
+  Author: Saurav Negi
+========================================================================== */
 
+/*==========================================================================
+  Include files
+========================================================================== */
+#include <stdio.h>    // Standard I/O operations
+#include <stdlib.h>   // Standard library functions
+#include <string.h>   // String manipulation functions
+#include <syslog.h>   // System logging functions
+
+/* Description: writeToFile
+ * Writes a string to a file.
+ *
+ * Parameters:
+ * writefile : The name of the file to write to.
+ * writestr  : The string to write to the file.
+ *
+ * Return Type:
+ * int : Returns 0 on success, 1 on failure.
+ */
+int writeToFile(char *writefile, char *writestr);
+
+/* Description: main
+ * The main function of the program. It handles command-line arguments, opens a connection
+ * to the system log, writes a string to a file, logs messages, and closes the connection to the log.
+ *
+ * Parameters:
+ * argc : The number of command-line arguments.
+ * argv : An array of strings containing the command-line arguments.
+ *
+ * Return Type:
+ * int : Returns 0 on success, 1 on failure.
+ */
 int main(int argc, char *argv[]) {
-
+    
+    // Open a connection to the system log
     openlog("log", LOG_PID, LOG_USER);
     
     // Check if both arguments are provided
     if (argc != 3) {
-        //fprintf(stderr, "Usage: %s <writefile> <writestr>\n", argv[0]);
         syslog(LOG_ERR, "Usage: ./writer <writefile> <writestr>\n");
-        
-        //exit(EXIT_FAILURE);
         return 1;
     }
 
-    // Assign arguments to variables
+    // Assign command line arguments to variables
     char *writefile = argv[1];
     char *writestr = argv[2];
 
-    // Check if writefile is not empty
+    // Check if 'writefile' argument is not empty
     if (writefile == NULL || strlen(writefile) == 0) {
-        //fprintf(stderr, "Error: 'writefile' argument is empty.\n");
         syslog(LOG_ERR, "Error: 'writefile' argument is empty.\n");
-        
-        //exit(EXIT_FAILURE);
         return 1;
     }
 
-    // Check if writestr is not empty
+    // Check if 'writestr' argument is not empty
     if (writestr == NULL || strlen(writestr) == 0) {
-        //fprintf(stderr, "Error: 'writestr' argument is empty.\n");
         syslog(LOG_ERR, "Error: 'writestr' argument is empty.\n");
-        
-        //exit(EXIT_FAILURE);
         return 1;
     }
 
+    // Call the writeToFile function to handle writing to the file
+    int result = writeToFile(writefile, writestr);
+
+    // Log the message with syslog
+    syslog(LOG_DEBUG, "Writing '%s' to '%s'", writestr, writefile);
+    
+    // Close the connection to the system log
+    closelog();
+
+    // Exit with success or failure status
+    return result;
+}
+
+/* Description: writeToFile
+ * Writes a string to a file.
+ *
+ * Parameters:
+ * writefile : The name of the file to write to.
+ * writestr  : The string to write to the file.
+ *
+ * Return Type:
+ * int : Returns 0 on success, 1 on failure.
+ */
+int writeToFile(char *writefile, char *writestr) {
     // Open the file for writing
     FILE *file = fopen(writefile, "w");
     if (file == NULL) {
-        //fprintf(stderr, "Error: Failed to open file '%s' for writing.\n", writefile);
-        syslog(LOG_ERR, "Error: Failed to open file.");
-            
-        //exit(EXIT_FAILURE);
-        return 1;
+        syslog(LOG_ERR, "Error: Failed to open file '%s' for writing.\n", writefile);
+        return 1; // Return failure status
     }
 
     // Write the content to the file
     if (fprintf(file, "%s", writestr) < 0) {
-        //fprintf(stderr, "Error: Failed to write to file '%s'.\n", writefile);
-        syslog(LOG_ERR, "Error: Failed to write to file.");
-        
+        syslog(LOG_ERR, "Error: Failed to write to file '%s'.\n", writefile);
         fclose(file);
-        
-        //exit(EXIT_FAILURE);
-        return 1;
+        return 1; // Return failure status
     }
 
     // Close the file
     fclose(file);
 
-    // Log the message with syslog
-    //openlog("writer", LOG_PID, LOG_USER);
-    syslog(LOG_DEBUG, "Writing %s to %s", writestr, writefile);
-    closelog();
-
-    // Exit with success status
-    return 0;
+    return 0; // Return success status
 }
